49b2dfa95fe1fbf2ed5b4034f9f98d3f838ffb91
Ankur Gera <Ankur_Gera@external.mckinsey.com>
2020-01-30 01:30:22 -0500

    Converted normal movie ratings to star ratings like IMDB

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
-  belongs_to :user	
-  MAX_RATING = 10	
+  belongs_to :user
+  belongs_to :movie
+
   validates :rating, :inclusion => { in: (1..MAX_RATING).map {|rate| rate} }, :allow_nil => false
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+MAX_RATING = 10
